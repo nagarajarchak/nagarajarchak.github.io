8889eaec399a2b63929fb9d2ab2632d3ef20bf2b
nagarajarchak <archak.nagaraj@gmail.com>
2020-12-02 20:11:28 +0530

    Added about and projects

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-const swup = new Swup();
+$(function(){
+    $(".write").typed({
+        strings: ["Singapore","Melbourne","Gold Coast","Kuala Lampur","Bangalore","Sydney",
+        "Johor Bahru","Kodaikanal","Mysore","Bali","Chennai","Bangkok","Mumbai","Tiruchirappalli"],
+        typeSpeed: 50,
+        backSpeed: 20,
+        loop: true
+    });
+});

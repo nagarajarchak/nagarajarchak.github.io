40fcb7279489a826dd5610bdae013709babbc0f3
nagarajarchak <archak.nagaraj@gmail.com>
2023-07-16 18:02:09 +1000

    Clean up

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $(".write").typed({
-        strings: ["Singapore","Melbourne","Gold Coast","Kuala Lampur","Bangalore","Sydney",
-        "Johor Bahru","Kodaikanal","Mysore","Bali","Chennai","Bangkok","Mumbai","Tiruchirappalli"],
-        typeSpeed: 50,
-        backSpeed: 20,
-        loop: true
-    });
-});

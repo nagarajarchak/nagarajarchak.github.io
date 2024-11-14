a6f385b3e93c217f419bb737c22e9dc535d8f042
nagarajarchak <archak.nagaraj@gmail.com>
2024-11-14 15:53:52 +0530

    Made website mobile responsive

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ function toggleMode() {
     var mode = document.querySelector('.fa-solid')
     mode.classList.toggle("fa-moon");
     mode.classList.toggle("fa-sun");
-  
+
+    var mobMode = document.querySelector('.mobile-ld')
+    mobMode.classList.toggle("fa-moon");
+    mobMode.classList.toggle("fa-sun");
+
     var body = document.body;
     var scy = document.querySelector('.scy')
     var sci = document.querySelector('.sci')
